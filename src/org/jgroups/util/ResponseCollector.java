2b0ba5787640f1b3b48aadd1df0efefb1c7517d5
Bela Ban <belaban@yahoo.com>
2009-04-20 12:51:16 +0000

    new files

---
+++ src/org/jgroups/util/ResponseCollector.java
@@ -0,0 +1,145 @@
+package org.jgroups.util;
+
+import org.jgroups.Address;
+import org.jgroups.annotations.GuardedBy;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.TimeUnit;
+
+/** Similar to AckCollector, but collects responses, not just acks. Note that, once done, the object cannot be
+ * reused, but a new instance should be created. Null is not a valid key.
+ * @author Bela Ban
+ * @version $Id: ResponseCollector.java,v 1.1.2.1 2009/04/20 12:51:18 belaban Exp $
+ */
+public class ResponseCollector<T> {
+    @GuardedBy("lock")
+    private final Map<Address,T> responses;
+    private final Lock lock=new ReentrantLock(false);
+    private final Condition cond=lock.newCondition();
+
+
+    /**
+     *
+     * @param members List of members from which we expect responses
+     */
+    public ResponseCollector(Collection<Address> members) {
+        if(members == null)
+            throw new IllegalArgumentException("members cannot be null");
+        responses=new HashMap<Address,T>(members.size());
+        for(Address mbr: members)
+            responses.put(mbr, null);
+    }
+
+    public void add(Address member, T data) {
+        if(member == null)
+            return;
+        lock.lock();
+        try {
+            if(responses.containsKey(member)) {
+                responses.put(member, data);
+                cond.signalAll();
+            }
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void suspect(Address member) {
+        if(member == null)
+            return;
+        lock.lock();
+        try {
+            if(responses.remove(member) != null)
+                cond.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean hasAllResponses() {
+        lock.lock();
+        try {
+            for(Map.Entry<Address,T> entry: responses.entrySet()) {
+                if(entry.getValue() == null)
+                    return false;
+            }
+            return true;
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public Map<Address,T> getResults() {
+        return Collections.unmodifiableMap(responses);
+    }
+
+    public int size() {
+        lock.lock();
+        try {
+            return responses.size();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    /**
+     * Waits until all responses have been received, or until a timeout has elapsed.
+     * @param timeout Number of milliseconds to wait max. This value needs to be greater than 0, or else
+     * it will be adjusted to 2000
+     * @return boolean True if all responses have been received within timeout ms, else false (e.g. if interrupted)
+     */
+    public boolean waitForAllResponses(long timeout) {
+        if(timeout <= 0)
+            timeout=2000L;
+        long end_time=System.currentTimeMillis() + timeout;
+        long wait_time;
+
+        lock.lock();
+        try {
+            while(!hasAllResponses()) {
+                wait_time=end_time - System.currentTimeMillis();
+                if(wait_time <= 0)
+                    return false;
+                try {
+                    cond.await(wait_time, TimeUnit.MILLISECONDS);
+                }
+                catch(InterruptedException e) {
+                    Thread.currentThread().interrupt(); // set interrupt flag again
+                    return false;
+                }
+            }
+            return true;
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void stop() {
+        lock.lock();
+        try {
+            responses.clear();
+            cond.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb=new StringBuilder();
+        sb.append(responses).append(", complete=").append(hasAllResponses());
+        return sb.toString();
+    }
+}
---
+++ tests/junit-functional/org/jgroups/tests/ResponseCollectorTest.java
@@ -0,0 +1,121 @@
+
+package org.jgroups.tests;
+
+
+import junit.framework.TestCase;
+import org.jgroups.util.AckCollector;
+import org.jgroups.util.Util;
+import org.jgroups.util.ResponseCollector;
+import org.jgroups.TimeoutException;
+import org.jgroups.Address;
+import org.jgroups.stack.IpAddress;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+import java.net.UnknownHostException;
+
+
+/**
+ * @author Bela Ban
+ * @version $Id: ResponseCollectorTest.java,v 1.1.2.1 2009/04/20 12:51:16 belaban Exp $
+ */
+public class ResponseCollectorTest extends TestCase {
+
+    public ResponseCollectorTest(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    public void testAdd() {
+        ResponseCollector<Integer> coll=new ResponseCollector<Integer>(createMembers(3));
+        coll.add(new IpAddress(1000), 1);
+        System.out.println("coll = " + coll);
+        assertEquals(3, coll.size());
+        assertFalse(coll.hasAllResponses());
+        coll.add(new IpAddress(3000), 3);
+        coll.add(new IpAddress(2000), 2);
+        System.out.println("coll = " + coll);
+        assertEquals(3, coll.size());
+        assertTrue(coll.hasAllResponses());
+    }
+
+    public void testAddNonExistentKeys() {
+        ResponseCollector<Integer> coll=new ResponseCollector<Integer>(createMembers(2));
+        coll.add(new IpAddress(1000), 1);
+        System.out.println("coll = " + coll);
+        assertEquals(2, coll.size());
+        assertFalse(coll.hasAllResponses());
+        coll.add(new IpAddress(3000), 3); // will get dropped
+        coll.add(new IpAddress(2000), 2);
+        System.out.println("coll = " + coll);
+        assertEquals(2, coll.size());
+        assertTrue(coll.hasAllResponses());
+    }
+
+
+    public void testWaitForAllResponses() {
+        final ResponseCollector<Integer> coll=new ResponseCollector<Integer>(createMembers(3));
+        boolean rc=coll.waitForAllResponses(500);
+        assertFalse(rc);
+
+        new Thread() {
+            public void run() {
+                coll.add(new IpAddress(1000), 1);
+                Util.sleep(500);
+                coll.add(new IpAddress(2000), 2);
+                coll.add(new IpAddress(3000), 3);
+            }
+        }.start();
+
+        rc=coll.waitForAllResponses(700);
+        System.out.println("coll = " + coll);
+        assertTrue(rc);
+        assertTrue(coll.hasAllResponses());
+    }
+
+    public void testWaitForAllResponsesAndTimeout() {
+        final ResponseCollector<Integer> coll=new ResponseCollector<Integer>(createMembers(3));
+
+        new Thread() {
+            public void run() {
+                coll.add(new IpAddress(1000), 1);
+                Util.sleep(500);
+                coll.add(new IpAddress(2000), 2);
+                coll.add(new IpAddress(3000), 3);
+            }
+        }.start();
+
+        boolean rc=coll.waitForAllResponses(400);
+        System.out.println("coll = " + coll);
+        assertFalse(rc);
+        assertFalse(coll.hasAllResponses());
+    }
+
+
+    private static Collection<Address> createMembers(int num) {
+        List<Address> retval=new ArrayList<Address>(num);
+        int cnt=1000;
+        for(int i=0; i < num; i++) {
+            retval.add(new IpAddress(cnt));
+            cnt+=1000;
+        }
+        return retval;
+    }
+
+
+    public static void main(String[] args) {
+        String[] testCaseName={ResponseCollectorTest.class.getName()};
+        junit.textui.TestRunner.main(testCaseName);
+    }
+
+}
